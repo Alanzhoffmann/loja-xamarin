9cfcb7e148b14b016279df4ab062a3d35d4b7fd7
Alan Zelaquett Hoffmann <desenvolvimento6@softin.com.br>
2019-01-15 17:59:11 -0200

    Inicio da implementação do WebRequest

---
+++ Store/Store.Repositorio/Helper/WebRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Store.Repositorio.Helper
+{
+    public sealed class WebRequest
+    {
+        private WebRequest()
+        {
+        }
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        private static readonly Lazy<WebRequest> _lazy = new Lazy<WebRequest>(() => new WebRequest());
+        public static WebRequest Instancia => _lazy.Value;
+
+        public async Task<T> Retornar<T>(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var encoded = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
